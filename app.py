b895dfaf59b2944c769ac55fa77ab56fe5d0ec49
WeiChen-Lin <asdfg09487@gmail.com>
2021-05-15 22:44:22 +0800

    0515_week4 assignment -> Login

--- app.py
+++ app.py
@@ -1,20 +1,30 @@
 from flask import *
-
-import sys , json
+import sys , json, jwt, datetime
 sys.path.append("./data")
 
-from SQLoperation import MySQLCon
+from SQLoperation import MySQLCon, User_MySQLCon
 from dotenv import dotenv_values
 
 config = dotenv_values(".env")
 
-config["port"] = int(config["port"])
+sqlConfig = {
+    "host" : config["host"],
+    "port" : int(config["port"]),
+    "user" : config["user"],
+    "password" : config["password"],
+    "db" : config["db"],
+    "charset" : config["charset"]
+}
 
-sqlObject = MySQLCon(config)
+
+sqlObject = MySQLCon(sqlConfig)
+UsersqlObject = User_MySQLCon(sqlConfig)
 app=Flask(__name__) 
 
 app.config["JSON_AS_ASCII"]=False
 app.config["TEMPLATES_AUTO_RELOAD"]=True
+app.config["SECRET_KEY"] = config["secret_key"]
+
 
 # Pages
 @app.route("/")
@@ -50,7 +60,169 @@ def AttrwithKey():
 def AttrwithId(id):
 	
     return json.dumps(sqlObject.fromIdSearchAttr(id) , ensure_ascii=False )
-	
+
+@app.route("/api/user" , methods=["POST", "PATCH", "GET", "DELETE"])
+def User():
+    #登入方面
+    if request.method == 'PATCH':
+        
+        data = request.get_json()
+
+        if (data["email"] != "") & (data["password"] != ""):
+            
+            email = data["email"]
+            password = data["password"]
+
+            email_check , password_check = UsersqlObject.SignInUserCheck(email , password)
+            
+            print(email_check , password_check)
+
+            if (email_check[0] == 1):
+
+                if password_check:
+
+                    if password == password_check[0]: 
+
+                        expire = datetime.datetime.utcnow() + datetime.timedelta(minutes=30)
+
+                        response = make_response( jsonify({"ok":True}) , 200)
+
+                        token  = jwt.encode( {"email":email , "exp": expire} , app.config["SECRET_KEY"])
+                        
+                        response.set_cookie(key="token" , value=token , expires=expire)
+
+                        response.headers["Content-Type"] = "application/json"
+                        
+                        return response
+                    
+                    else:
+                        response = make_response( jsonify({"error":True , "message":"帳號或密碼錯誤請重新輸入"}) , 400)
+                
+                        response.headers["Content-Type"] = "application/json"
+                        
+                        return response
+                
+                else:
+                    response = make_response( jsonify({"error":True , "message":"帳號或密碼錯誤請重新輸入"}) , 400)
+                
+                    response.headers["Content-Type"] = "application/json"
+                    
+                    return response
+
+            elif (email_check != 1) or (password_check != password) :
+                
+                response = make_response( jsonify({"error":True , "message":"帳號或密碼錯誤請重新輸入"}) , 400)
+                
+                response.headers["Content-Type"] = "application/json"
+                
+                return response
+            
+            else:
+
+                response = make_response( jsonify({"error":True , "message":"伺服器錯誤"}) , 500)
+                
+                response.headers["Content-Type"] = "application/json"
+                
+                return response
+    
+        else:
+            
+                response = make_response( jsonify({"error":True , "message":"請輸入帳號及密碼"}) , 500)
+                
+                response.headers["Content-Type"] = "application/json"
+                
+                return response
+
+    #註冊方面
+    elif request.method == "POST":
+
+        data = request.get_json()
+
+        email = data["email"]
+        password = data["password"]
+        username = data["username"]
+
+        if (email == "") or (password == "") or (username ==""):
+            response = make_response( jsonify({"error":True, "message": "每個欄位皆須填寫"}) , 400)    
+            response.headers["Content-Type"] = "application/json"
+            return response
+
+        else:
+
+            username_check , email_check = UsersqlObject.SignUpUserCheck(username , email)
+
+            if (username_check[0] == 0) & (email_check[0] == 0):
+                
+                #使用者資料新增進資料庫
+                UsersqlObject.tableInsertUser(email, password, username)
+
+                response = make_response( jsonify({"ok":True}) , 200)
+                
+                response.headers["Content-Type"] = "application/json"
+                
+                return response
+
+            elif (email_check[0] != 0):
+
+                response = make_response( jsonify({"error":True , "message":"您輸入的電子信箱已有人使用"}) , 400)
+                
+                response.headers["Content-Type"] = "application/json"
+                
+                return response            
+            
+            elif (username_check[0] != 0):
+
+                response = make_response( jsonify({"error":True , "message":"使用者名稱已有人使用"}) , 400)
+                
+                response.headers["Content-Type"] = "application/json"
+                
+                return response   
+
+            else:
+
+                response = make_response( jsonify({"error":True , "message":"伺服器內部錯誤"}) , 500)
+                
+                response.headers["Content-Type"] = "application/json"
+                
+                return response 
+
+    #登入取得使用者cookcie -> 不用重複登入
+    elif request.method == "GET":
+
+        cookie = request.cookies.get("token")
+        
+        if not cookie:
+            
+            response = make_response( jsonify({"data":None}) , 200)
+                
+            response.headers["Content-Type"] = "application/json"
+            
+            return response
+
+        data = jwt.decode(cookie, app.config["SECRET_KEY"] , algorithms=['HS256'])
+        
+        userinfo = {"id":UsersqlObject.getUserInfor(data["email"])[0], "email":UsersqlObject.getUserInfor(data["email"])[1], "name":UsersqlObject.getUserInfor(data["email"])[2] }
+        
+        response = make_response( jsonify({"data":userinfo}) , 200)
+        
+        response.headers["Content-Type"] = "application/json"
+        
+        return response
+
+    #登出方面(刪除連線)
+    elif request.method == "DELETE":
+
+        response = make_response( jsonify({"ok":True}) , 200)
+
+        response.set_cookie(key="token" , value="" , expires=0)
+
+        response.headers["Content-Type"] = "application/json"
+        
+        return response
+
+    #最後錯誤連線方法
+    else:
+        return {"error":"Invalid Connection"} , 500      
 
 app.run(port=3000 , debug=True)
 
---
+++ static/js/sign.js
@@ -0,0 +1,140 @@
+function SignInPage(){
+    let sign = document.getElementsByClassName("signbox");
+    let background = document.getElementsByClassName("background_layer");
+    sign[0].style.display = "block";
+    background[0].style.display = "block";  
+}
+
+function SignUpPage(){
+    let signup = document.getElementsByClassName("signupbox");
+    let sign = document.getElementsByClassName("signbox");
+    if(sign[0].style.display == "block"){
+        signup[0].style.display = "block";
+        sign[0].style.display = "none";
+    }
+}
+
+function BackToSign(){
+    let signup = document.getElementsByClassName("signupbox");
+    let sign = document.getElementsByClassName("signbox");
+    if(signup[0].style.display == "block"){
+        signup[0].style.display = "none";
+        sign[0].style.display = "block";
+    }
+}
+
+function closeJump(){
+    let signup = document.getElementsByClassName("signupbox");
+    let sign = document.getElementsByClassName("signbox");
+    let background = document.getElementsByClassName("background_layer");
+    signup[0].style.display = "none";
+    sign[0].style.display = "none";
+    background[0].style.display = "none";
+}
+
+function signIn(){
+    let position = document.getElementsByClassName("signform")[0];
+    let data = getform(position);
+    let data_to_python = JSON.stringify(data);
+
+    var requestURL = "http://52.76.36.230:3000/api/user";
+    var request = new XMLHttpRequest();
+
+    request.open("patch" , requestURL , true);
+    request.setRequestHeader('content-type', 'application/json');
+    request.send(data_to_python);
+    
+    request.onload = function(){
+        if (request.status >= 200 && request.status < 400){
+            closeJump();
+            let button = document.querySelector("#signin");
+            button.removeAttribute("onclick");
+            button.addEventListener("click", signOut, true);
+            let text = document.createTextNode("歡迎光臨 / 登出");
+            button.removeChild(button.childNodes[0]);
+            button.appendChild(text);
+        }else if(request.status >= 400){
+            let textarea = document.querySelector("body > div.signbox > form > div:nth-child(6)");
+            let text = document.createTextNode("帳號或密碼錯誤請重新輸入");
+            if(textarea.childNodes[0]){
+                textarea.removeChild(textarea.childNodes[0]);
+            };
+            textarea.appendChild(text);
+        }
+    };
+};
+
+function signUp(){
+    let position = document.getElementsByClassName("signupform")[0];
+    let data = getform(position);
+    let data_to_python = JSON.stringify(data);
+
+    let requestURL = "http://52.76.36.230:3000/api/user";
+    let request = new XMLHttpRequest();
+    request.open("POST" , requestURL , true);
+    request.setRequestHeader('content-type', 'application/json');
+    request.send(data_to_python);
+    
+    request.onload = function(){
+        if (request.status >= 200 && request.status < 400){
+            let textarea = document.querySelector("body > div.signupbox > form > div:nth-child(7)");
+            let text = document.createTextNode("註冊成功，請點擊<點此登入>進行登入");
+            if(textarea.childNodes[0]){
+                textarea.removeChild(textarea.childNodes[0]);
+            };
+            textarea.appendChild(text);
+
+        }else if(request.status >= 400){
+            
+            let json = JSON.parse(request.responseText);
+            let textarea = document.querySelector("body > div.signupbox > form > div:nth-child(7)");
+            let text = document.createTextNode(json.message);
+            if(textarea.childNodes[0]){
+                textarea.removeChild(textarea.childNodes[0]);
+            };
+            textarea.appendChild(text);
+        }
+    };
+}
+
+function signOut(){
+
+    let requestURL = "http://52.76.36.230:3000/api/user";
+    let request = new XMLHttpRequest();
+    request.onload = function(){
+        if (request.status >= 200){
+            let button = document.querySelector("#signin");
+            let text = document.createTextNode("登入/註冊");
+            button.removeChild(button.childNodes[0]);
+            button.appendChild(text);
+            button.removeEventListener("click", signOut, true);
+            button.addEventListener("click", SignInPage, true);
+        };
+    };
+    request.open("DELETE" , requestURL , true);
+    request.setRequestHeader('content-type', 'application/json');
+    request.send();
+};
+
+function signCheck(){
+
+    let requestURL = "http://52.76.36.230:3000/api/user";
+    let request = new XMLHttpRequest();
+    request.onload = function(){
+        if (request.status >= 200){
+            let json = JSON.parse(request.responseText);
+            if(json.data){
+
+                let button = document.querySelector("#signin");
+                button.removeAttribute("onclick");
+                button.addEventListener("click", signOut, true);
+                let text = document.createTextNode("歡迎光臨 / 登出");
+                button.removeChild(button.childNodes[0]);
+                button.appendChild(text); 
+            }
+        };
+    };
+    request.open("GET" , requestURL , true);
+    request.setRequestHeader('content-type', 'application/json');
+    request.send();
+}
